a0734f5eb8bfdbb8ae0e0669312cb01b82a7c212
Robbie Rushton <rushton8830@gmail.com>
2015-10-10 00:31:12 -0500

    Initial Commit

---
+++ src/SimpleCryp.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class SimpleCryp {
+
+    public static void main(String[] args) {
+	SimpleCryp sc = new SimpleCryp();
+	File eFile = new File("Test");
+	encrypt(eFile);
+    }
+    
+    private final static String chars = "abcdefghijklmnopqrstuvwxyz1234567890?!.,`~@#$%^&*()-_+=[]{}|:;\"'<> ";
+    private final static Map<Character, Character> encryptMap = new HashMap<Character, Character>();
+    private final static Map<Character, Character> decryptMap = new HashMap<Character, Character>();
+    private static int offset = 10;
+    
+    public SimpleCryp() {
+        fillHashMap();
+    }
+    
+    private void fillHashMap() {
+	char[] ch = chars.toCharArray();
+        for(int i = 0; i < chars.length(); i++) {
+	    encryptMap.put(ch[i], ch[(i + offset) % ch.length]);
+	    decryptMap.put(ch[(i + offset) % ch.length], ch[i]);
+        }
+    }
+    
+    public static void encrypt(File fileName) {
+	File encryptedFile = new File("Encrypted");
+	try{
+	    PrintWriter p = new PrintWriter(new FileWriter(encryptedFile));
+	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    String line;
+	    while((line = br.readLine()) != null) {
+		p.println(encrypt(line));
+	    }
+	    p.close();
+        }
+        catch (Exception e) {
+	    System.out.println(e);
+	    encryptedFile.delete();
+        }
+    }
+    
+    public static String encrypt(String s) {
+        char[] encryptString = s.toLowerCase().toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char c : encryptString) {
+	    sb.append(encryptMap.get(c));
+        }
+        
+        return sb.toString();
+    }
+    
+    public static void decrypt(File fileName) {
+        File decryptedFile = new File("Decrypted");
+	try{
+	    PrintWriter p = new PrintWriter(new FileWriter(decryptedFile));
+	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    String line;
+	    while((line = br.readLine()) != null) {
+		p.println(decrypt(line));
+	    }
+	    p.close();
+        }
+        catch (Exception e) {
+	    System.out.println(e);
+	    decryptedFile.delete();
+        }
+    }
+    
+    public static String decrypt(String s) {
+        char[] decryptString = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char c : decryptString) {
+	    sb.append(decryptMap.get(c));
+        }
+        
+        return sb.toString();
+    }
+    
+    public static void setOffset(int o) {
+        offset = o;
+    }
+}
